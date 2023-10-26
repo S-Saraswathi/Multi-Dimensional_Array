c53296d5f1f2088f265a95d6c06312b4c56f6e41
Saraswathi S <139524048+S-Saraswathi@users.noreply.github.com>
2023-10-27 00:52:48 +0530

    Update print_array.cpp

--- print_array.cpp
+++ print_array.cpp
@@ -22,3 +22,17 @@ int main() {
         cout<<endl;
     }
 }
+/*OUTPUT
+PS D:\Sem-3> cd "d:\Sem-3\Exp9\" ; if ($?) { g++ exp9_1.cpp -o exp9_1 } ; if ($?) { .\exp9_1 }
+Enter element-(00): 4
+Enter element-(01): 7
+Enter element-(02): 1
+Enter element-(10): 8
+Enter element-(11): 4
+Enter element-(12): 6
+Enter element-(20): 0
+Enter element-(21): 7
+Enter element-(22): 2
+4  7  1  
+8  4  6  
+0  7  2   */
